df13310d9ea997624fd1cab44c85d0bb9df1632e
Rahmat Agung Julians <rahmatagungj@Gmail.com>
2021-08-13 16:16:01 +0700

    fix layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,6 @@
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="px-4 mx-auto sm:px-6 xl:px-0 flex flex-col h-screen">
+    <div className="md:px-4 mx-auto sm:px-6 xl:px-0 flex flex-col h-screen">
       {children}
       <div className="flex-grow" />
       <footer className="p-5 border-t-2 border-opacity-20">
